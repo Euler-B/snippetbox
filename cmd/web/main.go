2a5df9c5ce67c2bc3cfbd94b6da0b31c54475dd5
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2024-03-09 18:50:07 -0400

    feat: :art: chapter 3
    added:  managing configurations settings, leveled logging, dependency injection, centralized error handling, isolating the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux := http.NewServeMux()
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+type appplication struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
-	log.Print("Servidor Funcionando en el Puerto :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+func main() {
+	addr := flag.String("addr", ":4000", "htto network address")
+	flag.Parse()
+
+	infolog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorlog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &appplication{
+		errorLog: errorlog,
+		infoLog:  infolog,
+	}
+
+	
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorlog,
+		Handler: app.routes(),
+	}
+
+	infolog.Printf("Servidor Funcionando en el Puerto %s", *addr)
+	err := srv.ListenAndServe()
+	errorlog.Fatal(err)
 }
